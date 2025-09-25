a25b4713862aafa98fe6ead93cbb701d33e2cd4f
shidifen12 <146832257+shidifen12@users.noreply.github.com>
2025-09-25 17:29:54 +0800

    Delete datasets directory

--- datasets/__init__.py
+++
@@ -1,9 +0,0 @@
-from .kitti_dataset import KITTIDataset
-from .sceneflow_dataset import SceneFlowDatset
-from .sota_dataset import SOTADataset
-
-__datasets__ = {
-    "sceneflow": SceneFlowDatset,
-    "kitti": KITTIDataset,
-    "sota": SOTADataset
-}
